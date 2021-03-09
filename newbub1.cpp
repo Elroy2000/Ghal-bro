f2f912e491c8070c78355432810894506e0b918e
Elroy <elroyfdes@gmail.com>
2021-03-08 23:44:43 -0800

    qwee

--- newbub1.cpp
+++ newbub1.cpp
@@ -19,22 +19,3 @@ void bubbleSort(int arr[], int n)
         if (arr[j] > arr[j+1])  
             swap(&arr[j], &arr[j+1]);  
 }  
-
-/* Function to print an array */
-void printArray(int arr[], int size)  
-{  
-    int i;  
-    for (i = 0; i < size; i++)  
-        cout << arr[i] << " ";  
-    cout << endl;  
-}  
-// Driver code  
-int main()  
-{  
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};  
-    int n = sizeof(arr)/sizeof(arr[0]);  
-    bubbleSort(arr, n);  
-    cout<<"Sorted array: \n";  
-    printArray(arr, n);  
-    return 0;  
-}  
